51cf014279888e3852a1922f07a9d316c67e40b6
Sid Karunaratne <skarunaratne@zendesk.com>
2016-12-14 15:20:42 +0800

    Run an instance if needed
    Name the instance
    Attach an EIP
    Correctly detect if an instance is already running

--- lambda.py
+++ lambda.py
@@ -1,16 +1,20 @@
 import boto3
 from datetime import datetime
+import time
 
 def main(event, context):
-	instance = create_instance(event)
-	tag_instance(instance, {
-		"Name": "plex-%s" % str(datetime.now()),
-		"Role": "plex-media-server"
-	})
+	# Create / Get instance
+	state = None
+	instance = get_instance_by_role('plex-media-server')
+	if instance:
+		state = instance.state['Name']
+	if not instance or state not in ['pending','running']:
+		state = "pending"
+		instance = create_instance(event)
 	return {
 		"statusCode": 200,
 		"headers": { },
-		"body": "Success"
+		"body": state
 	}
 
 def create_instance(event):
@@ -26,11 +30,45 @@ def create_instance(event):
 		SecurityGroupIds = event['security_groups'].split(' '),
 		SubnetId = event['subnet_id'],
 		InstanceInitiatedShutdownBehavior = "terminate",
-		MinCount=1,
-		MaxCount=1)
-	return reservation['Instances'][0]
+		MinCount = 1,
+		MaxCount = 1)
+	# Convert object into correct type
+	instance = get_instance_by_id(reservation['Instances'][0]['InstanceId'])
+	tag_instance(instance, {
+		"Name": "plex-%s" % str(datetime.now()),
+		"Role": "plex-media-server"
+	})
+	# Wait until the instance is running
+	state = instance.state['Name']
+	while state != "running":
+		time.sleep(0.5)
+		state = get_instance_by_id(instance.instance_id).state['Name']
+	client.associate_address(
+		InstanceId = instance.instance_id,
+		AllocationId = event['eip_allocation_id'],
+		AllowReassociation = False
+	)
+	return instance
 
 def tag_instance(instance, data):
 	ec2 = boto3.resource('ec2')
 	tags = [ {'Key':k,'Value':v} for k,v in data.iteritems() ]
-	ec2.create_tags(Resources=[instance['InstanceId']], Tags=tags)
+	ec2.create_tags(Resources=[instance.instance_id], Tags=tags)
+
+def get_instance_by_role(name):
+	ec2 = boto3.resource('ec2')
+	filters = [
+		{'Name':'instance-state-name','Values':['running','pending']},
+		{'Name':'tag:Role','Values':[name]}
+	]
+	instances = list(ec2.instances.filter(Filters=filters))
+	if len(instances) is 0:
+		return None
+	return instances[0]
+
+def get_instance_by_id(id):
+	ec2 = boto3.resource('ec2')
+	instances = list(ec2.instances.filter(InstanceIds=[id]))
+	if len(instances) is 0:
+		return None
+	return instances[0]
